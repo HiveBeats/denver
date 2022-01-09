20df713a64559639b5e9a3048dda0d032f846b95
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2022-01-09 12:22:26 +0700

    wip: find and replace template

--- include/templater.h
+++ include/templater.h
@@ -28,9 +28,50 @@ char smth[] = "CREATE USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT}';";
     }
     */
 
+char* process_line(char* line)
+{
+    char chr;
+    int i = 0;
+    int start_idx, end_idx;
+    
+    while(chr = line[i] != '\0')
+    {
+        //ищем старт темплейта
+        if (chr == '$' && line[i+1] == '{')
+        {
+            start_idx = i;
+            //нашли конец темплейта
+            while (chr = line[i] != '}') {
+                i++
+            }
+            end_idx = i;
+            //вытаскиваем имя темплейта
+            char* name;
+            name = malloc(sizeof(char) * ((end_idx - start_idx) + 1));
+            int ndx = 0;
+            for (int j = start_idx; j < end_idx; j++)
+            {
+                name[ndx] = line[j];
+                ndx++;
+            }
+            //получили значение на замену темплейту
+            envar_t* env = find_env(name);
+            char* env_value = env->value;
+
+            //конкатенируем...
+
+
+            //чистимся 
+            free(name);
+        }
+        i++;
+    }
+}
+
 void process_template(const char* filename, env_arr_t variables)
 {
     FILE* fp = fopen(filename, "rw");
+    //для каждой строки
     //читать символы в файле
     //дойти до символа $
     //если следующий символ {, то запомнить индекс символа доллар
