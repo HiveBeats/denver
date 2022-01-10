911c68be1da979fee6bc317cdb44d294a5d6e671
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2022-01-10 16:16:08 +0700

    refactor: extract processing module

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-clang -o denvlate -I/usr/local/include -L/usr/local/lib -I./include ./src/env_reader.c ./src/template.c ./src/main.c -std=c11 -pedantic
+clang -o denvlate -I/usr/local/include -L/usr/local/lib -I./include ./src/env_reader.c ./src/processor.c ./src/template.c ./src/main.c -std=c11 -pedantic
---
+++ include/processor.h
@@ -0,0 +1,8 @@
+#ifndef PROCESSOR_H_
+#define PROCESSOR_H_
+
+#include "env_reader.h"
+
+char* process_line(env_arr_t variables, const char* line);
+
+#endif
